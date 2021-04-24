ceb7519bbd1154ab91268ec5482a66f303cd36ee
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2021-04-24 11:06:49 +0100

    Gradle 7.0 and Android Gradle Plugin 4.1.3.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     compileOnly(gradleApi())
     implementation(kotlin("stdlib-jdk8", version = kotlinVersion))
     implementation(kotlin("gradle-plugin", version = kotlinVersion))
-    implementation("com.android.tools.build:gradle:4.1.0")
+    implementation("com.android.tools.build:gradle:4.1.3")
 }
