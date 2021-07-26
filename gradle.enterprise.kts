f6b873fb5ac01f44665353fba5154abd7226e025
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2021-07-26 22:13:11 +0100

    Revert "Append from file instead."
    This reverts commit 46e79d002d863b3e4a7242551c1a9b5eea463b66.
    
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- gradle.enterprise.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("com.gradle.build-scan").version("3.6.3")
-}
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
