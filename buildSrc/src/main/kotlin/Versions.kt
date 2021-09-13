71b8e46d0b3f91cf5922ed364d08e3fced583961
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2021-09-13 10:00:17 +0100

    Kotlin Coroutines 1.5.1
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -40,7 +40,7 @@ enum class Versions(
     JUNIT5("5.7.2", URL("https://junit.org/junit5/")),
     JUNIT5_PLATFORM("1.7.2", URL("https://junit.org/junit5/")),
     KOTLIN("1.5.30", URL("https://github.com/JetBrains/kotlin")),
-    KOTLIN_COROUTINES("1.5.0", URL("https://github.com/Kotlin/kotlinx.coroutines")),
+    KOTLIN_COROUTINES("1.5.1", URL("https://github.com/Kotlin/kotlinx.coroutines")),
     KOTLIN_TEST("1.4.32", URL("https://github.com/JetBrains/kotlin")),
     KTLINT("0.41.0", URL("https://github.com/pinterest/ktlint")),
     KTLINT_GRADLE_PLUGIN("10.1.0", URL("https://github.com/JLLeitschuh/ktlint-gradle")),
