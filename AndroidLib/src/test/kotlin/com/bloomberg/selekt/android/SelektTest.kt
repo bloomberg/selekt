4a70eb9cd62d4a8b2e4d493a7863a75ded4357bb
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2021-11-27 09:22:18 +0000

    SQLCipher 4.5.0.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- AndroidLib/src/test/kotlin/com/bloomberg/selekt/android/SelektTest.kt
+++ AndroidLib/src/test/kotlin/com/bloomberg/selekt/android/SelektTest.kt
@@ -42,12 +42,12 @@ internal class SelektTest {
 
     @Test
     fun libVersion() {
-        assertEquals("3.34.1", Selekt.sqliteLibVersion())
+        assertEquals("3.36.0", Selekt.sqliteLibVersion())
     }
 
     @Test
     fun libVersionNumber() {
-        assertEquals(3_034_001, Selekt.sqliteLibVersionNumber())
+        assertEquals(3_036_000, Selekt.sqliteLibVersionNumber())
     }
 
     @OptIn(Experimental::class)
