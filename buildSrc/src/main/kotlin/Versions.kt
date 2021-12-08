d5f518ed2ae8e173d9d4ab7c4cccd656a0428408
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2021-12-08 12:14:18 +0000

    Robolectric 4.7.2.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -47,7 +47,7 @@ enum class Versions(
     MOCKITO("3.11.2", URL("https://github.com/mockito/mockito")),
     MOCKITO_KOTLIN("3.2.0", URL("https://github.com/nhaarman/mockito-kotlin")),
     NEXUS_PLUGIN("1.1.0", URL("https://github.com/gradle-nexus/publish-plugin")),
-    ROBOLECTRIC("4.6.1", URL("https://github.com/robolectric/robolectric"));
+    ROBOLECTRIC("4.7.2", URL("https://github.com/robolectric/robolectric"));
 
     override fun toString() = version
 
