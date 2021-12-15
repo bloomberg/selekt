eb8b8314e527127c2fd5ab6de48fe363b52f3b39
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2021-12-15 12:04:37 +0000

    NDK 23.1.7779620.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -24,7 +24,7 @@ enum class Versions(
     ANDROID_BUILD_TOOLS("31.0.0", URL("https://developer.android.com/studio/releases/build-tools")),
     ANDROID_GRADLE_PLUGIN("7.0.4", URL("https://developer.android.com/tools/revisions/gradle-plugin.html")),
     ANDROID_LINT("30.0.2", URL("https://github.com/googlesamples/android-custom-lint-rules")),
-    ANDROID_NDK("23.0.7599858", URL("https://developer.android.com/ndk")),
+    ANDROID_NDK("23.1.7779620", URL("https://developer.android.com/ndk")),
     ANDROID_SDK("31", URL("https://developer.android.com/sdk")),
     ANDROIDX_LIVE_DATA("2.2.0", URL("https://developer.android.com/topic/libraries/architecture/livedata")),
     ANDROIDX_ROOM("2.2.5", URL("https://developer.android.com/jetpack/androidx/releases/room")),
