bae50c97bad8964c40af1da21646d5b34665a53f
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2022-01-04 18:45:07 +0000

    AssertJ 3.22.0.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -28,7 +28,7 @@ enum class Versions(
     ANDROID_SDK("31", URL("https://developer.android.com/sdk")),
     ANDROIDX_LIVE_DATA("2.2.0", URL("https://developer.android.com/topic/libraries/architecture/livedata")),
     ANDROIDX_ROOM("2.2.5", URL("https://developer.android.com/jetpack/androidx/releases/room")),
-    ASSERT_J("3.20.2", URL("https://joel-costigliola.github.io/assertj")),
+    ASSERT_J("3.22.0", URL("https://joel-costigliola.github.io/assertj")),
     CMAKE("3.18.1", URL("https://cmake.org")),
     DETEKT("1.18.0", URL("https://github.com/arturbosch/detekt")),
     DOKKA("1.6.10", URL("https://github.com/Kotlin/dokka")),
