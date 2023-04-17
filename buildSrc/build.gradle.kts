1007bb448fc7e4f0fce8d9d5fcbc494f8f7d17f3
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2023-04-17 16:39:21 +0100

    Android Gradle Plugin 7.4.2.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -38,5 +38,5 @@ gradlePlugin {
 
 dependencies {
     implementation(kotlin("gradle-plugin", version = kotlinVersion))
-    implementation("com.android.tools.build:gradle:7.4.1")
+    implementation("com.android.tools.build:gradle:7.4.2")
 }
