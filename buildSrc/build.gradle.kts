851daae75c3f5bbbefd5db7eeb0a3e784e32990a
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2024-02-04 18:20:25 +0000

    Android Gradle Plugin 8.2.2.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -38,5 +38,5 @@ gradlePlugin {
 
 dependencies {
     implementation(kotlin("gradle-plugin", version = kotlinVersion))
-    implementation("com.android.tools.build:gradle:8.2.0")
+    implementation("com.android.tools.build:gradle:8.2.2")
 }
