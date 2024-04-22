3b8c0f483f2af804e24d3dd22677349e36577fc5
Kenneth J. Shackleton <kshackleton1@bloomberg.net>
2024-04-22 21:29:39 +0100

    Mockito-Kotlin 5.3.1.
    Signed-off-by: Kenneth J. Shackleton <kshackleton1@bloomberg.net>

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -45,7 +45,7 @@ enum class Versions(
     KTLINT("0.45.2", URL("https://github.com/pinterest/ktlint")),
     KTLINT_GRADLE_PLUGIN("11.5.0", URL("https://github.com/JLLeitschuh/ktlint-gradle")),
     MOCKITO("5.10.0", URL("https://github.com/mockito/mockito")),
-    MOCKITO_KOTLIN("5.2.1", URL("https://github.com/mockito/mockito-kotlin")),
+    MOCKITO_KOTLIN("5.3.1", URL("https://github.com/mockito/mockito-kotlin")),
     NEXUS_PLUGIN("1.3.0", URL("https://github.com/gradle-nexus/publish-plugin")),
     QODANA_PLUGIN("0.1.12", URL("https://www.jetbrains.com/help/qodana/qodana-gradle-plugin.html")),
     ROBOLECTRIC_ANDROID_ALL("12.1-robolectric-8229987", URL("https://github.com/robolectric/robolectric"));
