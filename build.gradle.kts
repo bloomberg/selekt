e53c85e91700268a8ab62760346028358f7153ec
kshackleton1 <kshackleton1@bloomberg.net>
2026-01-03 07:44:47 +0000

    Increase code coverage lower bound.
    Signed-off-by: kshackleton1 <kshackleton1@bloomberg.net>

--- build.gradle.kts
+++ build.gradle.kts
@@ -246,7 +246,7 @@ koverReport {
         verify {
             rule("Minimal coverage") {
                 bound {
-                    minValue = 96
+                    minValue = 97
                     aggregation = AggregationType.COVERED_PERCENTAGE
                 }
             }
