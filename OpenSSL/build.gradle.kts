02ea149e8e05f9fb4e8af4e6dda39740faf597ee
kshackleton1 <kshackleton1@bloomberg.net>
2026-01-04 19:34:39 +0000

    Consistent flags with Android.
    Signed-off-by: kshackleton1 <kshackleton1@bloomberg.net>

--- OpenSSL/build.gradle.kts
+++ OpenSSL/build.gradle.kts
@@ -151,10 +151,14 @@ tasks.register<Exec>("configureHost") {
     commandLine("./config")
     args(
         "-fPIC",
-        "no-shared",
-        "no-idea", "no-camellia", "no-seed", "no-bf", "no-cast", "no-rc2", "no-rc4", "no-rc5",
-        "no-md2", "no-md4", "no-mdc2", "no-whirlpool", "no-rmd160",
-        "no-ssl3", "no-ecdh", "no-sock", "no-engine"
+        "-fstack-protector-all",
+        "no-idea", "no-camellia", "no-seed", "no-bf", "no-blake2", "no-cast", "no-rc2", "no-rc4", "no-rc5",
+        "no-md2", "no-rmd160", "no-md4", "no-ecdh", "no-sock", "no-ssl", "no-ssl3",
+        "no-chacha", "no-des", "no-dh", "no-dsa", "no-ec", "no-ecdsa", "no-ec2m", "no-ocb",
+        "no-tls", "no-dtls", "no-nextprotoneg", "no-poly1305", "no-rfc3779", "no-whirlpool",
+        "no-scrypt", "no-srp", "no-mdc2", "no-engine", "no-ts", "no-sse2", "no-sm2", "no-sm3",
+        "no-sm4", "no-ocsp", "no-cmac", "no-srtp", "no-shared", "no-comp", "no-ct", "no-cms",
+        "no-capieng", "no-deprecated", "no-autoerrinit", "no-stdio", "no-ui-console", "no-filenames"
     )
 }
 
