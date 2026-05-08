0ed99f1dc9213991f095ef48a0b8d40a5468f525
github-action-benchmark <github@users.noreply.github.com>
2026-05-08 13:04:13 +0000

    add JDBC Allocations (jmh) benchmark result for a60c181982033afe924595cee781feaf47282782

---
+++ benchmarks/jmh-alloc/data.js
@@ -0,0 +1,102 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1778245453116,
+  "repoUrl": "https://github.com/bloomberg/selekt",
+  "entries": {
+    "JDBC Allocations": [
+      {
+        "commit": {
+          "author": {
+            "name": "Ken Shackleton",
+            "username": "kennethshackleton",
+            "email": "kennethshackleton@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "a60c181982033afe924595cee781feaf47282782",
+          "message": "Merge pull request #914 from kennethshackleton/alloc-benchmarks\n\nAllocation benchmarks",
+          "timestamp": "2026-05-08T12:49:30Z",
+          "url": "https://github.com/bloomberg/selekt/commit/a60c181982033afe924595cee781feaf47282782"
+        },
+        "date": 1778245452425,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.selektBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"SIMPLE\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.selektBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"MIXED\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.selektBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"LARGE_BLOBS\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.selektReusedBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"SIMPLE\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.selektReusedBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"MIXED\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.selektReusedBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"LARGE_BLOBS\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.xerialBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"SIMPLE\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.xerialBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"MIXED\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.xerialBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"LARGE_BLOBS\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.xerialReusedBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"SIMPLE\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.xerialReusedBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"MIXED\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          },
+          {
+            "name": "com.bloomberg.selekt.jdbc.benchmarks.JdbcBatchBenchmark.xerialReusedBatchInsert ( {\"batchSize\":\"1000\",\"dataType\":\"LARGE_BLOBS\"} )",
+            "value": null,
+            "unit": null,
+            "extra": "iterations: undefined\nforks: undefined\nthreads: undefined"
+          }
+        ]
+      }
+    ]
+  }
+}
